d3ce2a2157e50d2e08b28d317f8e86532359cbeb
surendrakoritala <surendra.koritala88@gmail.com>
2021-08-11 20:12:41 -0400

    removed async

--- api/Search/index.js
+++ api/Search/index.js
@@ -74,86 +74,76 @@ module.exports = async function (context, req) {
 
     context.log(req);
 
-    (async () => {
-        try {
-            // Reading inputs from HTTP Request
-            let q = (req.query.q || (req.body && req.body.q));
-            const top = (req.query.top || (req.body && req.body.top));
-            const skip = (req.query.skip || (req.body && req.body.skip));
-            const filters = (req.query.filters || (req.body && req.body.filters));
-            const isSemantic = (req.query.isSemantic || (req.body && req.body.isSemantic));
-            const facets = readFacets(process.env["SearchFacets"]);
-            const searchQueryLanguage = (req.query.searchQueryLanguage || (req.body && req.body.searchQueryLanguage));
-            context.log(searchQueryLanguage);
-
-            // If search term is empty, search everything
-            if (!q || q === "") {
-                q = "*";
-            }
-
-            // Creating SearchOptions for query
-            var searchOptions;
-            if (isSemantic & q !== "*") {
-                searchOptions = {
-                    top: top,
-                    skip: skip,
-                    includeTotalCount: true,
-                    facets: Object.keys(facets),
-                    filter: createFilterExpression(filters, facets),
-                    queryType: "semantic",
-                    speller: "lexicon",
-                    queryLanguage: searchQueryLanguage || "en-us",
-                    answers: "extractive|count-3",
-                    searchFields: ["ProductDesc", "SyndigoProductName", "SyndigoMarketingDescription", "CategoryDesc"]
-                };
-            }
-            else {
-                searchOptions = {
-                    top: top,
-                    skip: skip,
-                    includeTotalCount: true,
-                    facets: Object.keys(facets),
-                    filter: createFilterExpression(filters, facets)
-                };
-            }
-
-
-            // Sending the search request
-            var searchResults = await client.search(q, searchOptions);
-
-            // Getting results for output
-            const output = [];
-            for await (const result of searchResults.results) {
-                output.push(result);
-            }
-
-            // Logging search results
-            context.log(searchResults.count);
-
-            // Creating the HTTP Response
-            context.res = {
-                // status: 200, /* Defaults to 200 */
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: {
-                    count: searchResults.count,
-                    results: output,
-                    facets: searchResults.facets,
-                    answers: searchResults.answers
-                }
-            };
-        } catch (error) {
-            context.log.error(error);
-
-            // Creating the HTTP Response
-            context.res = {
-                status: 400,
-                body: {
-                    innerStatusCode: error.statusCode || error.code,
-                    error: error.details || error.message
-                }
+    try {
+        // Reading inputs from HTTP Request
+        let q = (req.query.q || (req.body && req.body.q));
+        const top = (req.query.top || (req.body && req.body.top));
+        const skip = (req.query.skip || (req.body && req.body.skip));
+        const filters = (req.query.filters || (req.body && req.body.filters));
+        const isSemantic = (req.query.isSemantic || (req.body && req.body.isSemantic));
+        const facets = readFacets(process.env["SearchFacets"]);
+        const searchQueryLanguage = (req.query.searchQueryLanguage || (req.body && req.body.searchQueryLanguage));
+        context.log(searchQueryLanguage);
+        // If search term is empty, search everything
+        if (!q || q === "") {
+            q = "*";
+        }
+        // Creating SearchOptions for query
+        var searchOptions;
+        if (isSemantic & q !== "*") {
+            searchOptions = {
+                top: top,
+                skip: skip,
+                includeTotalCount: true,
+                facets: Object.keys(facets),
+                filter: createFilterExpression(filters, facets),
+                queryType: "semantic",
+                speller: "lexicon",
+                queryLanguage: searchQueryLanguage || "en-us",
+                answers: "extractive|count-3",
+                searchFields: ["ProductDesc", "SyndigoProductName", "SyndigoMarketingDescription", "CategoryDesc"]
             };
         }
-    })();
+        else {
+            searchOptions = {
+                top: top,
+                skip: skip,
+                includeTotalCount: true,
+                facets: Object.keys(facets),
+                filter: createFilterExpression(filters, facets)
+            };
+        }
+        // Sending the search request
+        var searchResults = await client.search(q, searchOptions);
+        // Getting results for output
+        const output = [];
+        for await (const result of searchResults.results) {
+            output.push(result);
+        }
+        // Logging search results
+        context.log(searchResults.count);
+        // Creating the HTTP Response
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: {
+                count: searchResults.count,
+                results: output,
+                facets: searchResults.facets,
+                answers: searchResults.answers
+            }
+        };
+    } catch (error) {
+        context.log.error(error);
+        // Creating the HTTP Response
+        context.res = {
+            status: 400,
+            body: {
+                innerStatusCode: error.statusCode || error.code,
+                error: error.details || error.message
+            }
+        };
+    }
 };
